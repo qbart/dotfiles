dfd2932a227dd4fd22fc02fafb9dc44f754e7ba5
bart <wojtowicz.bartlomiej@gmail.com>
2020-08-28 17:31:33 +0200

    Add WSL

---
+++ wsl/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install python3-pip nodejs
+sudo apt install tmux httpie screen ripgrep highlight ranger 
+sudo apt install golang cmake clang neovim cargo
+
+cargo install exa bat fd
+pip3 install pynvim
+
+
---
+++ wsl_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p ~/.config
+
+ln -s "$(pwd)/nvim" ~/.config/nvim -f
+ln -s "$(pwd)/ranger" ~/.config/ranger -f
+ln -s "$(pwd)/byhost" ~/.config/byhost -f
+
+ln -s "$(pwd)/.bash_aliases" ~/.bash_aliases -f
+#ln -s "$(pwd)/.bashrc" ~/.bashrc -f
+
+#ln -s "$(pwd)/.tmux.conf" ~/.tmux.conf -f
+
+rm ~/.config/ranger/ranger
+rm ~/.config/nvim/nvim
+rm ~/.config/byhost/byhost
