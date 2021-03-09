c24fb49dc978c1952ed59ecefce30c2043b4afe2
qbart <wojtowicz.bartlomiej@gmail.com>
2021-03-09 23:59:20 +0100

    update snips and aliases

--- asdf.sh
+++ asdf.sh
@@ -9,6 +9,8 @@ asdf plugin-add waypoint https://github.com/Banno/asdf-hashicorp.git
 asdf plugin-add boundary https://github.com/Banno/asdf-hashicorp.git
 asdf plugin-add gohugo https://bitbucket.org/mgladdish/asdf-gohugo
 asdf plugin-add crystal https://github.com/asdf-community/asdf-crystal.git
+asdf plugin-add caddy https://github.com/salasrod/asdf-caddy.git
+asdf plugin-add lua https://github.com/Stratus3D/asdf-lua.git
 asdf plugin-add ruby 
 asdf plugin-add elixir 
 asdf plugin-add erlang 
