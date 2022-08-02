a1d49382c0fd74e624b235155dc6625e8a470343
qbart <wojtowicz.bartlomiej@gmail.com>
2022-08-03 01:33:28 +0200

    Add vscode config

---
+++ setup-vscode.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -sf "$(pwd)/nvim-vscode/settings.json" ~/.config/"Code - OSS"/User/settings.json
+ln -sf "$(pwd)/nvim-vscode/keybindings.json" ~/.config/"Code - OSS"/User/keybindings.json
