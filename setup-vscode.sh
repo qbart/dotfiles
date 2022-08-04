22d11c9dc44244cb1f38f76600f6b59481c0f9c2
qbart <wojtowicz.bartlomiej@gmail.com>
2022-08-04 03:18:37 +0200

    Change code repate for vscode

--- setup-vscode.sh
+++ setup-vscode.sh
@@ -3,6 +3,8 @@
 if [[ "$OSTYPE" == 'darwin'* ]]; then
     ln -sf "$(pwd)/nvim-vscode/settings.json" ~/Library/"Application Support"/Code/User/settings.json
     ln -sf "$(pwd)/nvim-vscode/keybindings.json" ~/Library/"Application Support"/Code/User/keybindings.json
+
+    defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 else
     ln -sf "$(pwd)/nvim-vscode/settings.json" ~/.config/"Code - OSS"/User/settings.json
     ln -sf "$(pwd)/nvim-vscode/keybindings.json" ~/.config/"Code - OSS"/User/keybindings.json
