aabda70c01580c8a0aac3852c51a776ef1c0a613
qbart <wojtowicz.bartlomiej@gmail.com>
2022-10-15 21:58:04 +0200

    Add LF support

--- nvim/init.lua
+++ nvim/init.lua
@@ -37,6 +37,13 @@ require('packer').startup(function(use)
     -- Collection of configurations for built-in LSP client
     use 'neovim/nvim-lspconfig'                                                          
 
+    use { "lmburns/lf.nvim",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "akinsho/toggleterm.nvim"
+        }
+    }
+
     -- auto close brackets
     use {
         "windwp/nvim-autopairs",
@@ -289,6 +296,28 @@ require('Comment').setup {
     },
 }
 
+vim.g.lf_netrw = 1
+require("lf").setup({
+    default_actions = { -- default action keybindings
+        ["<C-k>"] = "split",
+        ["<C-l>"] = "vsplit",
+    },
+
+    winblend = 10, -- psuedotransparency level
+    dir = "", -- directory where `lf` starts ('gwd' is git-working-directory, "" is CWD)
+    direction = "float", -- window type: float horizontal vertical
+    border = "curved", -- border kind: single double shadow curved
+    height = 0.80, -- height of the *floating* window
+    width = 0.80, -- width of the *floating* window
+    escape_quit = true, -- map escape to the quit command (so it doesn't go into a meta normal mode)
+    focus_on_open = true, -- focus the current file when opening Lf (experimental)
+    mappings = true, -- whether terminal buffer mapping is enabled
+    tmux = false, -- tmux statusline can be disabled on opening of Lf
+
+    -- Layout configurations
+    layout_mapping = "<A-u>" -- resize window with this key
+})
+
 
 ----
 -- keys
@@ -310,6 +339,7 @@ vim.api.nvim_set_keymap('n', '<leader>g', [[:Telescope live_grep<CR>]], {})
 vim.api.nvim_set_keymap('n', '<leader>b', [[:Telescope buffers<CR>]], {})
 vim.api.nvim_set_keymap('n', '<leader>h', [[:Telescope help_tags<CR>]], {})
 vim.api.nvim_set_keymap('n', '<leader>f', [[:Telescope current_buffer_fuzzy_find<CR>]], {})
+vim.api.nvim_set_keymap('n', '<leader>s', [[:Telescope lsp_workspace_symbols<CR>]], {})
 -- TODO snippets
 -- line
 vim.api.nvim_set_keymap('n', 'n', [[:set number!<CR>]], {})
@@ -329,7 +359,7 @@ vim.keymap.set({'n','v'}, 'j', 'h', {noremap=true})
 vim.api.nvim_set_keymap('n', '<C-k>', '5j', {noremap=true})
 vim.api.nvim_set_keymap('n', '<C-l>', '5k', {noremap=true})
 -- open LF file manager (external dep)
-vim.api.nvim_set_keymap('n', '<C-e>', [[:Lf<CR>]], {})
+vim.api.nvim_set_keymap('n', "<C-e>", "<cmd>lua require('lf').start()<CR>", { noremap = true })
 -- save file
 vim.api.nvim_set_keymap('n', '<C-s>', [[:w<CR>]], {})
 -- quit
