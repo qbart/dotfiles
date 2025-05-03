71a30f26f4d3e087e2407a844afa14713b49545d
qbart <wojtowicz.bartlomiej@gmail.com>
2025-05-04 01:13:07 +0200

    snippets

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -26,8 +26,44 @@ return {
     requires = {
       { 'saadparwaiz1/cmp_luasnip' },
     },
-    config = function(_, opts)
+    config = function()
       local cmp = require('cmp')
+      local luasnip = require('luasnip')
+
+      local cmp_mapping = {
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          -- local copilot_suggestion = require("copilot.suggestion")
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+            -- elseif copilot_suggestion.is_visible() then
+            --   copilot_suggestion.accept()
+          -- elseif has_words_before() then
+          --   cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        -- ['<CR>'] = cmp.mapping.confirm({
+        --   behavior = cmp.ConfirmBehavior.Replace,
+        --   select = false,
+        -- }), -- accept only if select so you can create new line normally
+      }
 
       cmp.setup({
         preselect = cmp.PreselectMode.Item,
@@ -35,16 +71,10 @@ return {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
         },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        }),
+        mapping = cmp_mapping,
         snippet = {
           expand = function (args)
-            require('luasnip').lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
           end,
         },
         sources = cmp.config.sources({
@@ -92,6 +122,15 @@ return {
 
       local lspconfig = require('lspconfig')
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+      capabilities.textDocument.completion.completionItem.resolveSupport = {
+        properties = {
+          'documentation',
+          'detail',
+          'additionalTextEdits',
+        }
+      }
+
       local lsOpts = {
         capabilities = capabilities,
         on_attach = function(client, buffer)
@@ -223,8 +262,8 @@ return {
     requires = {
       { 'saadparwaiz1/cmp_luasnip' },
     },
-    config = function (_, opts)
-      require("luasnip.loaders.from_vscode").lazy_load()
+    config = function ()
+      require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
       require("luasnip.loaders.from_snipmate").lazy_load()
     end
   }
