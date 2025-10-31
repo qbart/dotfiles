a3a6686ab2972a233d94730cecd229dcaf42bcce
qbart <wojtowicz.bartlomiej@gmail.com>
2025-10-31 01:48:50 +0100

    new theme

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -15,6 +15,18 @@ return {
     end,
     },
 
+    {
+        "robitx/gp.nvim",
+        config = function()
+            local conf = {
+                -- For customization, refer to Install > Configuration in the Documentation/Readme
+            }
+            require("gp").setup(conf)
+
+            -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
+        end,
+    },
+
     -- auto pairs
     {
         "echasnovski/mini.pairs",
