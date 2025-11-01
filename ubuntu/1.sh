b5d98fd02e284d67962a4d5ab895dc5d5f38d0c6
qbart <wojtowicz.bartlomiej@gmail.com>
2025-11-01 21:14:05 +0100

    update vim & ubuntu

--- ubuntu/1.sh
+++ ubuntu/1.sh
@@ -3,7 +3,8 @@
 sudo apt install \
      vim git git-lfs cargo make fd-find curl cmake build-essential \
      htop tmux bat podman podman-compose flatpak qemu-system \
-     xclip xsel peek flameshot pkg-config ninja-build
+     xclip xsel peek flameshot pkg-config ninja-build \
+     sox libsox-fmt-mp3 luarocks tree-sitter-cli
 
 [ ! -f ~/.gitconfig ] && cp ../.gitconfig ~/.gitconfig
 [ ! -f ~/bin/fd ] && ln -s $(which fdfind) ~/bin/fd
