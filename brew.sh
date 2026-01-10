1bc4e37afe7c09bf740f4a5a364b87e6084096b8
qbart <git@qbart.dev>
2026-01-10 12:18:57 +0100

    fix: osx clang

--- brew.sh
+++ brew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-brew install curl wget ngrok/ngrok/ngrok tmux sox cmake btop gpg fzf
+brew install curl wget ngrok/ngrok/ngrok tmux sox cmake btop gpg fzf pkg-config ninja
 brew install --HEAD neovim
