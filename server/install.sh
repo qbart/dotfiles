e23d66654ed26e3727a689d191eee859f688b0cb
qbart <git@qbart.dev>
2026-01-30 15:50:20 +0100

    server

--- server/install.sh
+++ server/install.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-if command -v docker &> /dev/null
-then
-        echo "Docker already installed."
-else
-        apt update
-        apt -y install ca-certificates curl
-        install -m 0755 -d /etc/apt/keyrings
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-        chmod a+r /etc/apt/keyrings/docker.asc
-
-        # Add the repository to Apt sources:
-        echo \
-          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-          $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-          tee /etc/apt/sources.list.d/docker.list > /dev/null
-        apt update
-        apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-        groupadd docker
-fi
-
 aliases="$HOME/.bash_aliases"
 if [ ! -f "$aliases" ]; then
     touch $aliases
@@ -29,14 +9,21 @@ else
     echo "Aliases already added."
 fi
 
-if ! command -v caddy &> /dev/null; then
-    apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
-
-    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-
-    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-    apt update
-    apt -y install caddy
+if command -v podman &> /dev/null
+then
+    echo "Podman already installed."
 else
-    echo "Caddy is already installed."
+    apt update
+    apt -y install ca-certificates curl vim podman
+
+    # podman rootless
+    SYSCTL_FILE="/etc/sysctl.d/99-userns.conf"
+
+    touch "$SYSCTL_FILE"
+
+    grep -q '^kernel.unprivileged_userns_clone' "$SYSCTL_FILE" \
+        || echo 'kernel.unprivileged_userns_clone = 1' >> "$SYSCTL_FILE"
+
+    sysctl --system
 fi
+
--- server/nomad.sh
+++ server/nomad.sh
@@ -2,16 +2,41 @@
 set -e
 
 if ! which nomad >/dev/null 2>&1; then
-      wget -O - https://apt.releases.hashicorp.com/gpg \
-              | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    wget -O - https://apt.releases.hashicorp.com/gpg \
+        | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-            https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" \
-                | sudo tee /etc/apt/sources.list.d/hashicorp.list >/dev/null
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+        https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" \
+        | tee /etc/apt/sources.list.d/hashicorp.list >/dev/null
 
-          sudo apt update
-            sudo apt install -y nomad
-        else
-              echo "nomad already installed"
+    apt update
+    apt install -y nomad
+
+
+    # sysctl rules
+    SYSCTL_FILE="/etc/sysctl.d/bridge.conf"
+
+    mkdir -p /etc/sysctl.d
+    touch "$SYSCTL_FILE"
+
+    grep -q '^net.bridge.bridge-nf-call-arptables' "$SYSCTL_FILE" \
+        || echo 'net.bridge.bridge-nf-call-arptables = 1' >> "$SYSCTL_FILE"
+
+    grep -q '^net.bridge.bridge-nf-call-ip6tables' "$SYSCTL_FILE" \
+        || echo 'net.bridge.bridge-nf-call-ip6tables = 1' >> "$SYSCTL_FILE"
+
+    grep -q '^net.bridge.bridge-nf-call-iptables' "$SYSCTL_FILE" \
+        || echo 'net.bridge.bridge-nf-call-iptables = 1' >> "$SYSCTL_FILE"
+
+    sysctl --system
+
+    # cni
+    export ARCH_CNI=$( [ $(uname -m) = aarch64 ] && echo arm64 || echo amd64)
+    export CNI_PLUGIN_VERSION=v1.6.2
+    curl -L -o cni-plugins.tgz "https://github.com/containernetworking/plugins/releases/download/${CNI_PLUGIN_VERSION}/cni-plugins-linux-${ARCH_CNI}-${CNI_PLUGIN_VERSION}".tgz && \
+        mkdir -p /opt/cni/bin && \
+        tar -C /opt/cni/bin -xzf cni-plugins.tgz
+else
+    echo "nomad already installed"
 fi
 
